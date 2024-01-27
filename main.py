f234325e243055b151f9c836fea0f02be3e287e2
kanogame <wasdqert1222@gmail.com>
2024-01-27 22:39:42 +0700

    main interpreter

--- main.py
+++ main.py
@@ -29,4 +29,36 @@ for i in inp:
         streak = 1
         streakCommand = i
 
+cells = [0]
+pointer = 0
+
 #print("\n".join([str(i) + " " + cmds[i][0] + " " + str(cmds[i][1]) for i in range(len(cmds))]))
+
+def inter(commands, offset=0):
+    global pointer
+    i = 0
+    while i < len(commands):
+        command = commands[i]
+        match command[0]:
+            case "+":
+                cells[pointer] += command[1]
+            case "-":
+                cells[pointer] -= command[1]
+            case ">":
+                pointer += command[1]
+            case "<":
+                pointer -= command[1]
+            case ".":
+                for _ in range(command[1]):
+                    print(chr(cells[pointer]), end="")
+            case ",":
+                0
+            case "[":
+                while cells[pointer] >= 1:
+                    inter(commands[i + 1:command[1] - offset], i + 1)
+                i = command[1] - offset
+        if pointer > len(cells) - 1:
+            cells.append(0)
+        i += 1
+inter(cmds)
+print("")
