077ffcd10ff8cca90483e9c089e97b76fed3588c
kanogame <wasdqert1222@gmail.com>
2024-01-27 22:47:15 +0700

    refactor

--- main.py
+++ main.py
@@ -1,40 +1,38 @@
 #!/usr/bin/env python3
 import sys
 
-if len(sys.argv) < 1:
-    print("error")
-    exit(-1)
+def main():
+    if len(sys.argv) < 2:
+        print("error")
+        exit(-1)
+    cmds = parse_file(sys.argv[1])
+    interpret(cmds)
+    print("")
 
-inp = "".join([i for i in open(sys.argv[1])])
-cmds = []
-allowed = "<>+-,.[]"
-streak = 0
-streakCommand = ""
+def parse_file(path):
+    inp = "".join([i for i in open(path)])
+    cmds, callStack = [], []
+    allowed = "<>+-,.[]"
+    streak = 0
+    streakCommand = ""
 
-callStack = []
-
-for i in inp:
-    if i not in allowed:
-        continue
-    if i == "[":
-        callStack.append(len(cmds) + 1)
-    elif i == "]":
-        cmds[callStack[-1]][1] = len(cmds) + 1
-        callStack.pop()
-    if i == streakCommand:
-        streak += 1
-    else:
-        if streak > 0:
-            cmds.append([streakCommand, streak])
-        streak = 1
-        streakCommand = i
-
+    for i in inp:
+        if i not in allowed: continue
+        if i == "[": callStack.append(len(cmds) + 1)
+        elif i == "]":
+            cmds[callStack[-1]][1] = len(cmds) + 1
+            callStack.pop()
+        if i == streakCommand: streak += 1
+        else:
+            if streak > 0:
+                cmds.append([streakCommand, streak])
+            streak = 1
+            streakCommand = i
+    return cmds
 cells = [0]
 pointer = 0
 
-#print("\n".join([str(i) + " " + cmds[i][0] + " " + str(cmds[i][1]) for i in range(len(cmds))]))
-
-def inter(commands, offset=0):
+def interpret(commands, offset=0):
     global pointer
     i = 0
     while i < len(commands):
@@ -55,10 +53,11 @@ def inter(commands, offset=0):
                 0
             case "[":
                 while cells[pointer] >= 1:
-                    inter(commands[i + 1:command[1] - offset], i + 1)
+                    interpret(commands[i + 1:command[1] - offset], i + 1)
                 i = command[1] - offset
         if pointer > len(cells) - 1:
             cells.append(0)
         i += 1
-inter(cmds)
-print("")
+
+if __name__ == "__main__":
+    main()
